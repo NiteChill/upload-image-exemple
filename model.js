2643125ecc5a3ac69c166454860fc072e73ffda1
NiteChill <puissantachille@gmail.com>
2024-03-22 09:46:47 +0100

    siuuuu

--- model.js
+++ model.js
@@ -1,11 +1,14 @@
 var mongoose = require('mongoose');
-var imageSchema = new mongoose.Schema({
-  name: String,
-  desc: String,
-  img: {
-    data: Buffer,
-    contentType: String,
+var imageSchema = new mongoose.Schema(
+  {
+    name: String,
+    desc: String,
+    img: {
+      data: Buffer,
+      contentType: String,
+    },
   },
-});
+  { collection: 'lol' }
+);
 
 module.exports = mongoose.model('Image', imageSchema);
